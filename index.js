5986bd9e54f7556768366e942b1ba066d2e87431
Vu Nguyen <hello.vunguyen@gmail.com>
2020-11-02 21:48:11 -0600

    fixing styles

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const root = document.documentElement;
 const cards = [...document.querySelectorAll('.material-card')];
 
 const updateStyles = (event) => {
-  const dimenions = event.target.getBoundingClientRect();
-
-  root.style.setProperty('--mount-x', `{e.clientX - dimensions.x}px`);
-  root.style.setProperty('--mount-x', `{e.clientY - dimensions.y}px`);
+  const dimensions = event.target.getBoundingClientRect();
+  console.log('dimensions', dimensions)
+  console.log('root', root)
+  root.style.setProperty('--mouse-x', `${event.clientX - dimensions.x}px`);
+  root.style.setProperty('--mouse-y', `${event.clientY - dimensions.y}px`);
 };
 
 if (cards && cards.length > 0) {
   for (let card of cards) {
+    console.log(card)
     card.addEventListener('mouseenter', updateStyles);
     card.addEventListener('mouseleave', updateStyles);
   }
 }
+
+console.log('cards', cards)
